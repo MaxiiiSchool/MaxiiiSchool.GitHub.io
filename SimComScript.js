a47b44b5e51335cf3a733d189a565cf9223c6082
MaxiAmZocken <max@auto-heyer.de>
2024-11-10 20:24:37 +0100

    Changes to the whole page

--- SimComScript.js
+++
@@ -1,8 +0,0 @@
-function calculatePrice() {
-    var exchangeprice = parseFloat(document.getElementById("exchangeprice").value);
-    var percentage = parseFloat(document.getElementById("percentage").value);
-
-    var price = exchangeprice - ((percentage/100)*exchangeprice);
-
-    document.getElementById("result").textContent = "Total Cost: $" + price.toFixed(3);
-}
